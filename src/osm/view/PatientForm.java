4961d560defd099970c6248d90ef2d32d85b1e73
Izowirus <izowirus@gmail.com>
2017-03-28 21:04:31 +0200

    OSM

--- src/osm/view/PatientForm.java
+++ src/osm/view/PatientForm.java
@@ -1,14 +1,21 @@
 package osm.view;
 
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import osm.controller.PatientFormController;
 import osm.controller.TableController;
 import osm.model.Patient;
 
-public class PatientForm extends Pane implements PatientFormController { //TODO change Pane type
+public class PatientForm extends VBox implements PatientFormController { //TODO change Pane type
 	
 	private TableController tableController;
-
+	
+	Label nameLabel = new Label("Imiê:");
+	TextField nameField = new TextField();
+	
 	@Override
 	public void clearForm() {
 		// TODO Auto-generated method stub
